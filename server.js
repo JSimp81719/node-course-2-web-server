dbc89eae7155c234f816c66a30a7528fe0763012
Joseph Simpson <josephsimpson@Josephs-Mac-mini.local>
2017-02-27 18:14:15 -0500

    Update to page title of projects page

--- server.js
+++ server.js
@@ -60,7 +60,9 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-  res.render('projects.hbs');
+  res.render('projects.hbs', {
+    pageTitle: 'My Portfolio Page',
+  });
 });
 
 app.get('/bad', (req, res) => {
