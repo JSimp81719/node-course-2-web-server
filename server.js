38477100d6e5342b18a7a9e65a5425a26da3018d
Joseph Simpson <josephsimpson@Josephs-Mac-mini.local>
2017-02-27 18:06:01 -0500

    Added project link

--- server.js
+++ server.js
@@ -59,6 +59,10 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs');
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'There was an error.'
